5f44f9a0138bde2af0e9bae9fa1be6693ec0ecdf
Emily Lauren Roth <emilylroth@utexas.edu>
2019-11-13 11:12:40 -0600

    Added try catch to handle errors

--- src/main.java
+++ src/main.java
@@ -7,54 +7,50 @@ import java.util.regex.Pattern;
 
 public class main {
     public static void main(String[] args) throws Exception{
-        // Taking inputs inputs from txt file
-        // Assumptions: Input read in as file named input.txt
+        // Assumption: Input read in as file named input.txt
         URL path = main.class.getResource("input.txt");
         File file = new File(path.getFile());
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         conversions con = new conversions();
 
-        // Reading the file
-        // Assumptions: Output to console rather than to a file
+        // Assumption: Output to console rather than to a file
         String str;
         Pattern numAssignRegex = Pattern.compile("(\\w*) means ([IVXLCDM]*)");
         Pattern unitAssignRegex = Pattern.compile("(.*) units of (\\w*) are worth (\\d*) Credits");
-
         Pattern numValueRegex = Pattern.compile("how much is (.*)( [?])");
         Pattern creditValueRegex = Pattern.compile("how many Credits is (.*)( [?])");
 
         while ((str = br.readLine()) != null) {
             Matcher matcher;
-            if(numAssignRegex.matcher(str).matches()){
-                matcher = numAssignRegex.matcher(str);
-                matcher.find();
-                String unit = matcher.group(1);
-                String symbol = matcher.group(2);
-                con.numberTranslation(unit, symbol);
-            }
-            else if(unitAssignRegex.matcher(str).matches()){
-                matcher = unitAssignRegex.matcher(str);
-                matcher.find();
-                String units = matcher.group(1);
-                String material = matcher.group(2);
-                String credits = matcher.group(3);
-                con.materialTranslation(units, material, credits);
-            }
-            else if(numValueRegex.matcher(str).matches()){
-                matcher = numValueRegex.matcher(str);
-                matcher.find();
-                String numerals = matcher.group(1);
-                System.out.println(numerals + " is "+ con.numeralToDecimal(numerals));
-            }
-            else if (creditValueRegex.matcher(str).matches()){
-                // find how many credits something is worth
-                matcher = creditValueRegex.matcher(str);
-                matcher.find();
-                String input = matcher.group(1);
-                System.out.println(input + " is " + con.mineralValToDecimal(input) + " Credits");
-            }
-            else{
+            try {
+                if (numAssignRegex.matcher(str).matches()) {
+                    matcher = numAssignRegex.matcher(str);
+                    matcher.find();
+                    String unit = matcher.group(1);
+                    String symbol = matcher.group(2);
+                    con.numberTranslation(unit, symbol);
+                } else if (unitAssignRegex.matcher(str).matches()) {
+                    matcher = unitAssignRegex.matcher(str);
+                    matcher.find();
+                    String units = matcher.group(1);
+                    String material = matcher.group(2);
+                    String credits = matcher.group(3);
+                    con.materialTranslation(units, material, credits);
+                } else if (numValueRegex.matcher(str).matches()) {
+                    matcher = numValueRegex.matcher(str);
+                    matcher.find();
+                    String numerals = matcher.group(1);
+                    System.out.println(numerals + " is " + con.numeralToDecimal(numerals));
+                } else if (creditValueRegex.matcher(str).matches()) {
+                    matcher = creditValueRegex.matcher(str);
+                    matcher.find();
+                    String input = matcher.group(1);
+                    System.out.println(input + " is " + con.mineralValToDecimal(input) + " Credits");
+                } else {
+                    System.out.println("I have no idea what you are talking about");
+                }
+            } catch (Exception e){
                 System.out.println("I have no idea what you are talking about");
             }
         }
